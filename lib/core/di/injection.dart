a57c040618f59eb78c851d35ce1d0ebf212c3047
9369702788 <67014896+9369702788@users.noreply.github.com>
2026-07-31 23:25:42 +0300

    Create injection.dart

---
+++ lib/core/di/injection.dart
@@ -0,0 +1,9 @@
+// lib/core/di/injection.dart (Simplified working version)
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> setupLocator() async {
+  // Add your services here when they're ready
+  // For now, just register basic services
+}
