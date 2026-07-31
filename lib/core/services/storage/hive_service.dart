90bb8ac6e5067b68d0dede776b33679519929a46
9369702788 <67014896+9369702788@users.noreply.github.com>
2026-07-31 23:53:29 +0300

    Update hive_service.dart

--- lib/core/services/storage/hive_service.dart
+++ lib/core/services/storage/hive_service.dart
@@ -1,13 +1,14 @@
-// lib/core/services/storage/hive_service.dart (Simplified working version)
 import 'package:hive_flutter/hive_flutter.dart';
 
 class HiveService {
-  static const String settingsBox = 'settings_box';
+  static const String settingsBoxName = 'settings_box';
   
   static Future<void> init() async {
     await Hive.initFlutter();
-    await Hive.openBox(settingsBox);
+    await Hive.openBox(settingsBoxName);
   }
-  
-  Box get settingsBox => Hive.box(settingsBox);
+
+  Box get settingsBox => Hive.box(settingsBoxName);
+
+  Box getBox(String name) => Hive.box(name);
 }
