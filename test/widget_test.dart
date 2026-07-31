9db1e58d003c6eea1a26a20358fa21c7626970f6
9369702788 <67014896+9369702788@users.noreply.github.com>
2026-07-31 23:46:13 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wirdi/main.dart';
+
+void main() {
+  testWidgets('WirdiApp smoke test initialization', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const WirdiApp());
+
+    // Verify application root shell mounts correctly
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+}
