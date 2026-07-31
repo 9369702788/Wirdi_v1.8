e437e9e7decf4cd9c4564017f282a22a347a4f57
9369702788 <67014896+9369702788@users.noreply.github.com>
2026-07-31 23:22:41 +0300

    Create dashboard_page.dart

---
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -0,0 +1,79 @@
+// lib/features/dashboard/presentation/pages/dashboard_page.dart
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Dashboard Page - Coming Soon'),
+      ),
+    );
+  }
+}
+
+// lib/features/quran/presentation/pages/quran_page.dart
+import 'package:flutter/material.dart';
+
+class QuranPage extends StatelessWidget {
+  const QuranPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Quran Page - Coming Soon'),
+      ),
+    );
+  }
+}
+
+// lib/features/azkar/presentation/pages/azkar_page.dart
+import 'package:flutter/material.dart';
+
+class AzkarPage extends StatelessWidget {
+  const AzkarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Azkar Page - Coming Soon'),
+      ),
+    );
+  }
+}
+
+// lib/features/prayer/presentation/pages/prayer_page.dart
+import 'package:flutter/material.dart';
+
+class PrayerPage extends StatelessWidget {
+  const PrayerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Prayer Times - Coming Soon'),
+      ),
+    );
+  }
+}
+
+// lib/features/tasbeeh/presentation/pages/tasbeeh_page.dart
+import 'package:flutter/material.dart';
+
+class TasbeehPage extends StatelessWidget {
+  const TasbeehPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Tasbeeh - Coming Soon'),
+      ),
+    );
+  }
+}
